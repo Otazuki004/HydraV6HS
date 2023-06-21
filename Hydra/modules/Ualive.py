66e0704733ccd80af877e223b29bb96663c55776
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2023-06-21 07:47:35 +0530

    V-6

--- Hydra/modules/Ualive.py
+++ Hydra/modules/Ualive.py
@@ -2,16 +2,15 @@ from pyrogram import __version__ as pyro
 from pyrogram import filters
 from Hydra import pub as bot
 from Hydra import prefix, IMG, gamerhs
-
+from Hydra import OWNER_NAME as own
 
 @bot.on_message(filters.command("alive", prefixes=prefix) & filters.user(5965055071))
 async def alive(_, m):
-    you = bot.get_me()
     await m.reply_photo(
         IMG, caption = f"""~  𝐻𝑦𝑑𝑟𝑎 𝑆𝑦𝑠𝑡𝑒𝑚:**
 ━━━━━━━━━━━━━━━━━━━
 
-❥ **𝑂𝑊𝑁𝐸𝑅**: {you}
+❥ **𝑂𝑊𝑁𝐸𝑅**: {own}
 ❥ **𝑉𝐸𝑅𝑆𝐼𝑂𝑁:** {pyro}
 ❥ **𝐺𝐴𝑀𝐸 𝑉𝐸𝑅𝑆𝐼𝑂𝑁:** {gamerhs}
 ❥ **𝑁𝐸𝑇𝑊𝑂𝑅𝐾:** @Hyper_Speed0
