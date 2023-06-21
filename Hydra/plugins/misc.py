77a7031234f442bd1229988137198cb72266abb5
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2023-06-21 21:48:54 +0530

    V-6

---
+++ Hydra/plugins/misc.py
@@ -0,0 +1,50 @@
+import requests
+from gpytranslate import Translator
+from pyrogram import filters
+from Hydra import prefix
+from Hydra import DEV_USERS
+from Hydra import pub as bot
+
+
+@bot.on_message(filters.command("ud", prefixes=prefix) & filters.user(DEV_USERS))
+async def ud(_, message):
+    if len(message.command) < 2:
+        await message.reply("ɢɪᴠᴇ ᴍᴇ ᴀ ᴛᴇxᴛ")
+        return
+    text = message.text.split(None, 1)[1]
+    results = requests.get(
+        f"https://api.urbandictionary.com/v0/define?term={text}"
+    ).json()
+    reply_text = f'**results: {text}**\n\n{results["list"][0]["definition"]}\n\n_{results["list"][0]["example"]}_'
+    ud = await message.reply_text("ғɪɴᴅɪɴɢ.. ᴅᴇғɪɴᴇ.")
+    await ud.edit_text(reply_text)
+
+
+trans = Translator()
+
+
+@bot.on_message(filters.command("tr", prefixes=prefix) & filters.user(DEV_USERS))
+async def translate(_, message) -> None:
+    reply_msg = message.reply_to_message
+    if not reply_msg:
+        await message.reply_text("[ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ ᴛʀᴀɴsʟᴀᴛᴇ ɪᴛ!](https://telegra.ph/Lang-Codes-03-19-3)")
+        return
+    if reply_msg.caption:
+        to_translate = reply_msg.caption
+    elif reply_msg.text:
+        to_translate = reply_msg.text
+    try:
+        args = message.text.split()[1].lower()
+        if "//" in args:
+            source = args.split("//")[0]
+            dest = args.split("//")[1]
+        else:
+            source = await trans.detect(to_translate)
+            dest = args
+    except IndexError:
+        source = await trans.detect(to_translate)
+        dest = "en"
+    translation = await trans(to_translate, sourcelang=source, targetlang=dest)
+    reply = f"**Translated from {source} to {dest}**:\n" f"`{translation.text}`"
+    await message.delete()
+    await reply_msg.reply_text(reply)
