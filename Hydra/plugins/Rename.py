d601a17f781be31c09a7eeb433f1738321597ad6
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2023-06-22 19:25:52 +0530

    V-6

---
+++ Hydra/plugins/Rename.py
@@ -0,0 +1,16 @@
+import os
+from pyrogram import filters
+from Hydra import prefix
+from Hydra import pub as Hydra
+from Hydra import DEV_USERS as hss
+
+@Hydra.on_message(filters.command("rename", prefixes=prefix) & filters.user(hss))
+async def rename(_, message):
+  try:
+    filename = message.text.replace(message.text.split("rename")[0], "")
+    if reply := message.reply_to_message:
+      x = await message.reply_text("Downloading.....")
+      path = await reply.download(file_name=filename)
+      await x.edit("Uploading.....")
+      await message.reply_document(path)
+      await os.remove(path)￼Enter
