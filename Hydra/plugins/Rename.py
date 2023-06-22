23eec99934b114d9852c83649ea8af48dd1e9ed4
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2023-06-22 20:06:11 +0530

    V-6

--- Hydra/plugins/Rename.py
+++ Hydra/plugins/Rename.py
@@ -7,9 +7,12 @@ from Hydra import DEV_USERS as hss
 @Hydra.on_message(filters.command("rename", prefixes=prefix) & filters.user(hss))
 async def rename(_, message):
   try:
-    filename = message.text.replace(message.text.split("rename")[0], "")
-    if reply := message.reply_to_message:
-      x = await message.reply_text("Downloading.....")
-      path = await reply.download(file_name=filename)
-      await x.edit("Uploading.....")
-      await message.reply_document(path)
+    filename = message.text.replace(message.text.split("rename")[1], "")
+
+    except Exception as e:
+        print(e)
+      if reply := message.reply_to_message:
+        x = await message.reply_text("Downloading.....")
+        path = await reply.download(file_name=filename)
+        await x.edit("Uploading.....")
+        await message.reply_document(path)
