b0638ddf71c858ffdfb8ef82cebb16932bcdac02
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2023-06-22 19:12:22 +0530

    V-6

--- Hydra/plugins/join_leave.py
+++ Hydra/plugins/join_leave.py
@@ -9,7 +9,7 @@ async def join_chat(_, message):
     if len(message.command) < 2:
         message.reply_text("ɢɪᴠᴇ ᴀ ᴊᴏɪɴ ɢʀᴏᴜᴘ ᴜsᴇʀɴᴀᴍᴇ ᴏʀ ɪɴᴠɪᴛᴇ  ʟɪɴᴋ")
         return
-    link = await message.text.split("join")[1]
+    link = message.text.split("join")[1]
     await bot.join_chat(link)
     await message.reply_text(f"Successfully joined")
 
@@ -19,7 +19,7 @@ async def leave_chat(_, message):
     if len(message.command) < 2:
         message.reply_text("ɢɪᴠᴇ ᴀ ʟᴇғᴛ ɢʀᴏᴜᴘ ᴜsᴇʀɴᴀᴍᴇ ᴏʀ ɪɴᴠɪᴛᴇ  ʟɪɴᴋ")
         return
-    link = await message.text.split("leave")[1]
+    link = message.text.split("leave")[1]
     await bot.leave_chat(link)
     chat = await bot.get_chat(link)
     await message.reply_text(f"Successfully left")
