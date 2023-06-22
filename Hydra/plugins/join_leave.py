588972c8a09de3846acbab5f3f0173d4675afdbc
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2023-06-22 18:47:13 +0530

    V-6

--- Hydra/plugins/join_leave.py
+++ Hydra/plugins/join_leave.py
@@ -9,11 +9,10 @@ async def join_chat(_, m):
     if len(m.command) < 2:
         m.reply_text("ɢɪᴠᴇ ᴀ ᴊᴏɪɴ ɢʀᴏᴜᴘ ᴜsᴇʀɴᴀᴍᴇ ᴏʀ ɪɴᴠɪᴛᴇ  ʟɪɴᴋ")
         return
-    link = m.text.split(" ")[1]
+    link = await m.text.split(" ")[1]
     await bot.join_chat(link)
-    chat = bot.get_chat(link)
-    name = m.chat.title
-    await m.reply_text(f"Successfully joined {name}")
+    chat = await bot.get_chat(link)
+    await m.reply_text(f"Successfully joined")
 
 
 @bot.on_message(filters.command("leave", prefixes=prefix) & filters.user(uuu))
@@ -21,8 +20,7 @@ async def leave_chat(_, m):
     if len(m.command) < 2:
         m.reply_text("ɢɪᴠᴇ ᴀ ʟᴇғᴛ ɢʀᴏᴜᴘ ᴜsᴇʀɴᴀᴍᴇ ᴏʀ ɪɴᴠɪᴛᴇ  ʟɪɴᴋ")
         return
-    link = m.text.split(" ")[1]
+    link = await m.text.split(" ")[1]
     await bot.leave_chat(link)
-    chat = bot.get_chat(link)
-    name = m.chat.title
-    await m.reply_text(f"Successfully left {name}")
+    chat = await bot.get_chat(link)
+    await m.reply_text(f"Successfully left")
