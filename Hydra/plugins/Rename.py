cf0d7daa455eea5e65eea0708de34a3c79fbab4d
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2023-06-22 19:51:58 +0530

    Update Rename.py

--- Hydra/plugins/Rename.py
+++ Hydra/plugins/Rename.py
@@ -13,5 +13,3 @@ async def rename(_, message):
       path = await reply.download(file_name=filename)
       await x.edit("Uploading.....")
       await message.reply_document(path)
-      os.remove(path)
-      #hs
