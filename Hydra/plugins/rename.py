98b0dcfb64d386db54ea10553f6d061d15d46691
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2023-06-23 18:59:23 +0530

    V-6

--- Hydra/plugins/rename.py
+++ Hydra/plugins/rename.py
@@ -4,17 +4,35 @@ from Hydra import prefix as HANDLER
 from Hydra import pub as Hydra
 from Hydra import DEV_USERS as OWNER_ID
 
-@Hydra.on_message(filters.command("rename", prefixes=HANDLER) & filters.usedef rename(_, message):
+async def FileType(message):
+    if message.document:
+        type = message.document.mime_type
+        return ["txt" if type == "text/plain" else type.split("/")[1]][0]
+    elif message.photo:
+          return "jpg"
+    elif message.animation:
+          return message.animation.mime_type.split("/")[1]
+    elif message.video:
+         return message.video.mime_type.split("/")[1]
+    else:
+         return False
 
+@Hydra.on_message(filters.command("rename",prefixes=HANDLER) & filters.user(OWNER_ID))
+async def rename(_, message):
     try:
-        filename = message.text.replace(message.text.split(" ")[0], "")
-
-    except Exception as e:
-        print(e)
-
-    if reply := message.reply_to_message:
-        x = message.reply_text("Downloading.....")
-        path = reply.download(file_name=filename)
-        x.edit("Uploading.....")
-        message.reply_document(path)
-        os.remove(path)
+       filename = message.text.split(None,1)[1]
+    except:
+        name = "Hydra"
+        try:
+          if (await FileType(message=message.reply_to_message)) != False:
+               filetype = await FileType(message=message.reply_to_message)
+        except Exception as e:
+               return await message.reply_text(f"Error: `{e}`")
+        filename = "{name}.{filetype}".format(name=name, filetype=filetype)
+    msg = await message.reply_text("⬇️ File has downloading...")
+    path = await message.reply_to_message.download(file_name=filename)
+    await msg.edit_text("⬆️ File has uplaoding")
+    await message.reply_document(document=path, thumb=thumb_id)
+    await msg.delete()
+    os.remove(path)
+    return
