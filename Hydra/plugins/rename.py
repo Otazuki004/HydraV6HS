ff413ff598ca774e9ae4602a554e27cc4352cb88
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2023-06-23 18:53:30 +0530

    V-6

--- Hydra/plugins/rename.py
+++ Hydra/plugins/rename.py
@@ -4,18 +4,17 @@ from Hydra import prefix as HANDLER
 from Hydra import pub as Hydra
 from Hydra import DEV_USERS as OWNER_ID
 
-@Hydra.on_message(filters.command("rename", prefixes=HANDLER) & filters.user(OWNER_ID))
-async def rename(_, message):
+@Hydra.on_message(filters.command("rename", prefixes=HANDLER) & filters.usedef rename(_, message):
 
     try:
-        filename = await message.text.replace(message.text.split(" ")[0], "")
+        filename = message.text.replace(message.text.split(" ")[0], "")
 
     except Exception as e:
         print(e)
 
     if reply := message.reply_to_message:
-        x = await message.reply_text("Downloading.....")
-        path = await reply.download(file_name=filename)
-        await x.edit("Uploading.....")
-        await message.reply_document(path)
+        x = message.reply_text("Downloading.....")
+        path = reply.download(file_name=filename)
+        x.edit("Uploading.....")
+        message.reply_document(path)
         os.remove(path)
